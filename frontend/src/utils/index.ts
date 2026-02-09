74ca10d1d3883c09d2ee1c9b74c236128e567d54
sanmipaul <sanmipaul1@gmail.com>
2026-02-09 05:04:52 +0100

    feat: export transaction utilities from utils index

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,3 +1,7 @@
 export { hexToUtf8, getWalletConnectUri, getRequestParams, isNativeApp, handleRedirect } from './walletkit-helpers';
 export { validateEnvironmentVariables, logEnvironmentValidation } from './env-validator';
 export { logger } from './logger';
+export { retryWithBackoff } from './retry';
+export { validateTransactionAmount, validateContractAddress, validateTransactionDetails } from './transactionValidator';
+export { TransactionErrorHandler } from './transactionErrorHandler';
+export { TransactionRecovery } from './transactionRecovery';
