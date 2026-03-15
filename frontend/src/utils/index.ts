2046c8ad0fb1ad9ff43e33f732933ad771728d63
sanmipaul <sanmipaul1@gmail.com>
2026-03-15 09:21:12 +0100

    chore(errors): export ContractErrorMapper and error code types from utils index

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,4 +1,7 @@
 export { hexToUtf8, getWalletConnectUri, getRequestParams, isNativeApp, handleRedirect } from './walletkit-helpers';
+export { ContractErrorMapper, ContractError } from './contractErrorMapper';
+export type { ContractErrorDescriptor, ContractErrorMap, AllContractErrors } from './contractErrorCodes';
+export { ALL_CONTRACT_ERRORS } from './contractErrorCodes';
 export { validateEnvironmentVariables, logEnvironmentValidation } from './env-validator';
 export { logger } from './logger';
 export * from './connectionUtils';
