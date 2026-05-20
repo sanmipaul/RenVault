042b4619981ddd695d57dd1c090d6cffb8596194
sanmipaul <sanmipaul1@gmail.com>
2026-05-20 10:30:27 +0100

    feat: add createAnalyticsSuite factory to analytics barrel for easy wiring

--- staking/analytics/index.js
+++ staking/analytics/index.js
@@ -6,4 +6,16 @@ const { RewardsAnalytics } = require('../rewardsAnalytics');
 const { StakingStats } = require('../stakingStats');
 const { RewardsExporter } = require('../rewardsExporter');
 
-module.exports = { RewardsHistory, RewardsAnalytics, StakingStats, RewardsExporter };
+/**
+ * Factory: create a fully-wired analytics suite from a StakingManager instance.
+ * @param {import('../stakingManager').StakingManager} stakingManager
+ */
+function createAnalyticsSuite(stakingManager) {
+  const history = new RewardsHistory();
+  const analytics = new RewardsAnalytics(history);
+  const stats = new StakingStats(stakingManager, history);
+  const exporter = new RewardsExporter(history);
+  return { history, analytics, stats, exporter };
+}
+
+module.exports = { RewardsHistory, RewardsAnalytics, StakingStats, RewardsExporter, createAnalyticsSuite };
