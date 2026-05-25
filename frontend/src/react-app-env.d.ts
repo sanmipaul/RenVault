1a478efe38dd61c3fda7ed390b3bece1615b479e
sanmipaul <sanmipaul1@gmail.com>
2026-05-25 22:16:14 +0100

    docs: update documentation

---
+++ frontend/src/react-app-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react-scripts" />
+
+// Declare missing modules for optional hardware wallet support
+declare module '@ledgerhq/hw-transport-webusb';
+declare module '@ledgerhq/hw-app-stacks';
